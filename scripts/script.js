70b34c09f4884e08bbfb55ef8460bdefc214c904
volchetski <59860314+volchetski@users.noreply.github.com>
2020-05-23 19:39:56 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+function Aducation(){
+  document.getElementById('content').innerHTML="<img src='img/aducation.jpg'>"
+}
+
+function Job(){
+  document.getElementById('content').innerHTML= "<img src='img/job.jpg'>"
+}
+
+function AboutMe() {
+  document.getElementById('content').innerHTML= "<img src='img/me.jpg'>"
+}
+
+function Target() {
+  document.getElementById('content').innerHTML= "<img src='img/target.jpg'>"
+}
+
+function Avatar(){
+    document.getElementById('content').innerHTML="<img src='img/myAvatar.jpg' class='myAvatar'>"
+}
